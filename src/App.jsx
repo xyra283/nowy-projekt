9c18d6f4a9b1265b65b093471d7cd4b7ad2c3405
Xyra <miko2833@interia.pl>
2023-08-09 21:46:33 +0200

    Poczatek

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import "./sass/app.scss";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+function Header() {
+  return (
+    <div className="nav-wrapper">
+      <img src="https://via.placeholder.com/100" alt="Giphy Logo" />
+
+      <nav id="">
+        <ul>
+          <li>
+            <a href="#">Trending</a>
+          </li>
+          <li>
+            <a href="#">Categories</a>
+          </li>
+          <li>
+            <a href="#">Favorites</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
